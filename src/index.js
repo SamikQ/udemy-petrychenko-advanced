2e5aa69a20703ce18b37df58310b245b349dd0cc
SamikQ <samokhvalov.dmitriy@gmail.com>
2024-02-15 16:51:24 +0200

    randomChar realized

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/style.scss';
-import MarvelService from './services/MarvelService';
 import App from './components/app/App';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name))); //Беремо массив всіх наших персонажів і виводимо імена
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
