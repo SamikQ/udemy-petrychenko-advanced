2c3589002d8af9876881ba2a83b579589e14c0db
SamikQ <samokhvalov.dmitriy@gmail.com>
2024-03-01 16:29:34 +0200

    offset charlist added

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -8,23 +8,40 @@ class CharList extends Component {
   state = {
     charList: [],
     loading: true,
-    error: false
+    error: false,
+    newItemLoading: false,
+    offset: 210
   };
 
   marvelService = new MarvelService();
 
   componentDidMount() {
-    this.marvelService
-      .getAllCharacters()
-      .then(this.onCharListLoaded)
-      .catch(this.onError);
+    this.onRequest();
   }
 
-  onCharListLoaded = (charList) => {
+  onRequest = (offset) => {
+    // метод для обробки запитів від користувача (клік на кнопку для завантаження більше персонажів)
+    this.onCharListLoading();
+    this.marvelService
+      .getAllCharacters(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  };
+
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false
+      newItemLoading: true,
     });
+  };
+
+  onCharListLoaded = (newCharList) => {
+    this.setState(({offset, charList }) => ({
+      // створюємо кол-бек функцію. В нас стейт залежить від попереднього стейта через довантаження нових персонажів.Це означає, що ми повертаємо обєкт з цієї функції
+      charList: [...charList, ...newCharList], // беремо старий стейт чарліст s розгортаємо його, і додаємо новий масив НьюЧарЛіст
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9
+    }));
   };
 
   onError = () => {
@@ -37,22 +54,29 @@ class CharList extends Component {
   onRenderList = (arr) => {
     const elements = arr.map((item) => {
       const { id, name, thumbnail } = item;
-      let imgStyle = { 'objectFit': 'cover' };
-      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-        imgStyle = { 'objectFit': 'unset' };
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
       }
       return (
-        <li className={`char__item`} key={id} onClick={() => this.props.onCharSelected(id)}>
+        <li
+          className={`char__item`}
+          key={id}
+          onClick={() => this.props.onCharSelected(id)}
+        >
           <img src={thumbnail} alt={name} style={imgStyle} />
           <div className="char__name">{name}</div>
         </li>
       );
     });
     return <ul className="char__grid">{elements}</ul>;
-  }
+  };
 
   render() {
-    const { charList, loading, error } = this.state;
+    const { charList, loading, error, offset, newItemLoading } = this.state;
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
 
@@ -64,7 +88,10 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         {content}
-        <button className="button button__main button__long">
+        <button 
+        className="button button__main button__long"
+        disabled={newItemLoading}
+        onClick={() => this.onRequest(offset)}>
           <div className="inner">load more</div>
         </button>
       </div>
